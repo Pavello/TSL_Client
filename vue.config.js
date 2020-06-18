36315729b84262fb0fd004ca88a011574e30aefd
Pawel Kania <pawel.kania3011@gmail.com>
2020-06-18 14:35:48 +0200

    Build path updated

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  outputDir: path.resolve(__dirname, '../server/public'),
+  outputDir: path.resolve(__dirname, '../TSL_Server/public'),
   devServer: {
     proxy: "http://localhost:3000"
   }
